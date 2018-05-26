63b46452876d2e5ba3fccd40b88540b20f823f21
zizle <zizle_lin@163.com>
2018-05-26 09:34:18 +0800

    获取音乐源地址

--- music_downloader.py
+++ music_downloader.py
@@ -1,3 +1,21 @@
 # _*_ coding:utf-8 _*_
-# 可以下载付费音乐的下载器, 不过就是比较麻烦, 要从酷狗源网页试听, 检查其网页源代码
+# 可以下载付费音乐的下载器, 不过就是比较麻烦, 要从源网页试听, 检查其网页源代码
 # 提取出含有.mp3的url地址
+import re
+
+
+def get_music_url():
+    src_url = input('请输入含有.mp3的URl地址:')
+    # 用正则匹配出url地址
+    match = re.match(r'.*(http://.*\.mp3).*', src_url)
+    if match:
+        music_url = match.group(1)
+        print('找到音乐的资源路径:{}'.format(music_url))
+        return music_url
+    else:
+        print('输入的地址有误！')
+
+
+if __name__ == '__main__':
+    music_name = input('请输入您要生成的文件名:')
+    get_music_url()
